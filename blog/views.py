1eb44116fd932aa2cd431566d235f428efd59ae7
yongjoon Kim <skysea259600@gmail.com>
2023-04-29 21:02:25 +0900

    FBV로 블로그 목록 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('pk') #order_by 받는 순서
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+          'posts': posts,
+        }
+    )
