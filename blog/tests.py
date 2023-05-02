0b5b73c3692f1580fbdea342c2e2b6a0b389f8eb
yongjoon Kim <skysea259600@gmail.com>
2023-05-02 19:56:34 +0900

    base모델 모듈화 하기

--- blog/tests.py
+++ blog/tests.py
@@ -64,11 +64,11 @@ class TestView(TestCase):
         soup=BeautifulSoup(response.content, 'html.parser')
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
         navbar = soup.nav
-        self.assertIn('Blog', navbar)
-        self.assertIn('About me', navbar)
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
 
         # 2.3 첫번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
-        self.assertIn(post_001.title, soup.title)
+        self.assertIn(post_001.title, soup.title.text)
         # 2.4 첫번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main_area')
         post_area = main_area.find('div', id='post_area')
