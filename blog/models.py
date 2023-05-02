47199988bb533bdc2e581ccb46b6d4950d4ce046
yongjoon Kim <skysea259600@gmail.com>
2023-05-03 01:10:55 +0900

    Category 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True) #allow_unicode 한글 가능하게하는 코드
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories' #목록 이름 바꾸기
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     hook_text = models.CharField(max_length=100,blank=True)
@@ -14,6 +24,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #CASCADE 사용자의 탈퇴와 같이 글이 지워짐 SET_NULL NULL로 초기화 해줌(NULL=True 추가 필요)
+    category =models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
@@ -26,3 +37,4 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
